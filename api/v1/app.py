427050a07d53ce05e2a1174334d49575d4164d5e
Julian <jose.torres122@outlook.com>
2022-02-05 13:23:29 -0400

    pep8 compliance

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,15 +18,13 @@ def handler(exception):
 
 
 if __name__ == "__main__":
-#    api_host = getenv('HBNB_API_HOST')
-#    api_port = getenv('HBNB_API_PORT')
-#    app.run(host=api_host, port=api_port, threaded=True)
-    try:
-        host = getenv("HBNB_API_HOST")
-    except:
-        host = "0.0.0.0"
-    try:
-        port = getenv("HBNB_API_PORT")
-    except:
-        port = "5000"
-    app.run(host=host, port=port, threaded=True)
+    api_host = getenv('HBNB_API_HOST')
+    api_port = getenv('HBNB_API_PORT')
+    if api_host is None:
+        app.run(host='0.0.0.0', port=api_port, threaded=True)
+    elif api_port is None:
+        app.run(host=api_host, port=5000, threaded=True)
+    elif api_host is None and api_port is None:
+        app.run(host='0.0.0.0', port=5000, threaded=True)
+    else:
+        app.run(host=api_host, port=api_port, threaded=True)

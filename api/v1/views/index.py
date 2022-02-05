036219736c31e960b3416683a2cf5ea6f39b5fec
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-02-05 19:28:28 +0000

    Add stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,23 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats', strict_slashes=False)
+def stats():
+
+    cls_dic = {"Amenity": "amenities", "City": "cities", "Place": "places",
+               "Review": "reviews", "State": "states", "User": "users"}
+    all_obj = {}
+
+    for key, value in cls_dic.items():
+        count = storage.count(key)
+        all_obj[value] = count
+    return jsonify(all_obj)
+

943a987001d723192ddc68fc853b12d12f21a9a8
Julian <jose.torres122@outlook.com>
2022-02-07 10:05:37 -0400

    Now be able to handle amenity objects

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''
+Amenity objects that handles all default RESTFul API actions
+'''
+
+
+from flask import jsonify, request, abort
+from AirBnB_clone_v3.models import amenity
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+
+
+@app_views.route('/api/v1/amenities', methods=['POST', 'GET'],
+                 strict_slashes=False)
+def all_States():
+    if request.method == 'POST':
+        new_amenity = None
+        try:
+            request_dict = request.get_json()
+            amenity_name = request_dict.get('name')
+            if amenity_name is None:
+                abort(400, description='Missing name')
+            new_amenity = Amenity(name=amenity_name)
+            new_amenity.save()
+            return jsonify(new_amenity.to_dict()), 201
+        except:
+            abort(400, description='Not a JSON')
+
+    # Retrieving all Amenity objects
+    all_amenities = storage.all('Amenity')
+    amenity_list = []
+    for amenity in all_amenities.values():
+        amenity_list.append(amenity.to_dict())
+    # returning all amenities in a list
+    return jsonify(amenity_list)
+
+
+@app_views.route('/api/v1/amenities/<amenity_id>',
+                 methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+def state_by_id(amenity_id):
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+    if request.method == 'DELETE':
+        storage.delete(amenity)
+        storage.save()
+        return jsonify({}), 200
+    elif request.method == 'PUT':
+        try:
+            request_dict = request.get_json()
+            request_dict['id'] = amenity.id
+            request_dict['created_at'] = amenity.created_at
+            amenity.__init__(**request_dict)
+            amenity.save()
+            return jsonify(amenity.to_dict()), 200
+        except:
+            abort(400, description='Not a JSON')
+    return jsonify(amenity.to_dict())
